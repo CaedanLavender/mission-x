94d25aaac4aa8beefc510510238f0510dd1d40b8
Caedan <caedanlavender@gmail.com>
2021-07-08 13:42:48 +1200

    Restructured images and used import instead of require

--- src/pages/TeacherDashboard.js
+++ src/pages/TeacherDashboard.js
@@ -1,10 +1,37 @@
 // CSS classnames follow rough BEM naming conventions: "__" to denote child item and "--" to denote modifier
 // http://getbem.com/naming/
 
-import { useState } from 'react';
 import './TeacherDashboard.css';
+import { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+
+// icon (*.png) imports
+
+import levelUpLogo from '../assets/global/star-logo.png';
+
+import nzFlag from '../assets/global/nz-flag.png';
+import maoriFlag from '../assets/global/maori-flag.png';
+
+import progressIconLight from '../assets/global/progress--icon--light.png';
+import progressIconDark from '../assets/global/progress--icon--dark.png';
+
+import studentProfilesIconLight from '../assets/global/student-profiles--icon--light.png';
+import studentProfilesIconDark from '../assets/global/student-profiles--icon--dark.png';
+
+import helpRequestsIconLight from '../assets/global/help-requests--icon--light.png';
+import helpRequestsIconDark from '../assets/global/help-requests--icon--dark.png';
+
+import projectSubmissionsIconLight from '../assets/global/project-submissions--icon--light.png';
+import projectSubmissionsIconDark from '../assets/global/project-submissions--icon--dark.png';
+
+import projectLibraryIconLight from '../assets/global/project-library--icon--light.png';
+
+import profileIcon from '../assets/global/profile--icon--light.png';
+import settingsIcon from '../assets/global/settings--icon--light.png';
+import logoutIcon from '../assets/global/logout--icon--light.png';
+
+
 
 export const TeacherDashboard = () => {
 	const [tab, setTab] = useState('Progress Tracker');
@@ -21,39 +48,39 @@ export const TeacherDashboard = () => {
 		{
 			name: "Progress Tracker",
 			icon: {
-				light: require("../assets/teacher-dashboard/icons/progress--icon--light.png").default,
-				dark: require("../assets/teacher-dashboard/icons/progress--icon--dark.png").default
+				light: progressIconLight,
+				dark: progressIconDark
 			},
 			linksto: null
 		},
 		{
 			name: "Student Profiles",
 			icon: {
-				light: require("../assets/teacher-dashboard/icons/student-profiles--icon--light.png").default,
-				dark: require("../assets/teacher-dashboard/icons/student-profiles--icon--dark.png").default
+				light: studentProfilesIconLight,
+				dark: studentProfilesIconDark
 			},
 			linksto: null
 		},
 		{
 			name: "Help Requests",
 			icon: {
-				light: require("../assets/teacher-dashboard/icons/help-requests--icon--light.png").default,
-				dark: require("../assets/teacher-dashboard/icons/help-requests--icon--dark.png").default
+				light: helpRequestsIconLight,
+				dark: helpRequestsIconDark
 			},
 			linksto: null
 		},
 		{
 			name: "Project Submissions",
 			icon: {
-				light: require("../assets/teacher-dashboard/icons/project-submissions--icon--light.png").default,
-				dark: require("../assets/teacher-dashboard/icons/project-submissions--icon--dark.png").default
+				light: projectSubmissionsIconLight,
+				dark: projectSubmissionsIconDark
 			},
 			linksto: null
 		},
 		{
 			name: "Project Library",
 			icon: {
-				light: require("../assets/teacher-dashboard/icons/project-library--icon--light.png").default,
+				light: projectLibraryIconLight,
 				dark: null
 			},
 			linksto: "/projectview"
@@ -63,15 +90,16 @@ export const TeacherDashboard = () => {
 	const bottomTabList = [
 		{
 			name: "Profile",
-			icon: require("../assets/teacher-dashboard/icons/profile--icon--light.png").default
+			icon: profileIcon
 		},
 		{
 			name: "Settings",
-			icon: require("../assets/teacher-dashboard/icons/settings--icon--light.png").default
+			icon: settingsIcon
+			// icon: require("../assets/global/settings--icon--light.png").default
 		},
 		{
 			name: "Log out",
-			icon: require("../assets/teacher-dashboard/icons/logout--icon--light.png").default
+			icon: logoutIcon
 		}
 	]
 
@@ -80,16 +108,17 @@ export const TeacherDashboard = () => {
 		// Wrapper is just a flex box with the main part (not the toolbar) set to have a flex-basis of 100% so that it tries to fill 100% but will accept anything lese (this will break if flex-shrink is changed, leave as default).
 		<div className="wrapper">
 			<div className="dashboard__toolbar">
-				<img src={require("../assets/teacher-dashboard/images/star-logo.png").default} alt="Levelup Works logo" />
+				<img src={levelUpLogo} alt="Levelup Works logo" />
 				<div className="dashboard__toolbar__flag-container">
-					<img src={require("../assets/teacher-dashboard/images/nz-flag.png").default} alt="Levelup Works logo" />
-					<img src={require("../assets/teacher-dashboard/images/maori-flag.png").default} alt="Levelup Works logo" />
+					<img src={nzFlag} alt="Levelup Works logo" />
+					<img src={maoriFlag} alt="Levelup Works logo" />
 				</div>
 			</div>
 			<div className="dashboard__container">
-				<div className="container__panel--left">
-					<div className="panel--left__item-wrapper">
+				<div className={`container__panel--left`}>
+					<div>
 						<div className="panel--left__profile">
+							<img src="" alt=""/>
 						</div>
 						{
 							tabList.map(item => (
@@ -98,19 +127,28 @@ export const TeacherDashboard = () => {
 									<div
 										// Below is a shortcircuit statment that looks for the tab state and the item matching, if true, then include an additional class that applies the 'selected' styling
 										className={`panel--left__item ${tab === item.name && "panel--left__item--selected"}`}
+										// Shortcircuit statement that sets the justifyContent property to center if tabOpen is not true
+										style={{justifyContent: !tabOpen && 'center'}}
 										// The ternary below is checking if there is a linksto property, if so, send null (this means that the tab is actually an external link and we do not want to run the tabchanging stuff), otherwise send the new tab into the changeTab function
 										onClick={() => changeTab(item.linksto ? null : item.name)}
 									>
 										{/* Ternary in src is saying that if the tab state matches 'this' tab, then do the dark icon, otherwise do the light icon */}
-										<img src={tab === item.name ? item.icon.dark : item.icon.light} alt={item.name + ""} />
-										{item.name}
+										<img
+											src={tab === item.name ? item.icon.dark : item.icon.light}
+											alt={item.name + ""}
+										/>
+										{/* Below will hide the name of the tab if the tabOpen state is false */}
+										{tabOpen && item.name}
 									</div>
 								</Link>
 							))
 						}
 					</div>
-					<div className={`panel--left__toggle ${tabOpen?'panel--left__toggle--open':'panel--left__toggle--closed'}`}>
-						<img src={require("../assets/teacher-dashboard/icons/left-pointing-caret.png").default} alt="arrow"/>
+					<div
+						className={`panel--left__toggle ${tabOpen?'panel--left__toggle--open':'panel--left__toggle--closed'}`}
+						onClick={()=>setTabOpen(!tabOpen)}
+					>
+						<img src={require("../assets/global/left-pointing-caret.png").default} alt="arrow"/>
 					</div>
 					<div className="panel--left__bottom-navigation">
 						{
