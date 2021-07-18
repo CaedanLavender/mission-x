e4e0f7efff500937e1930793ff284a5244fa4ec4
or <useyourtimewell@gmail.com>
2021-07-18 23:00:29 +1200

    fixed date display

--- src/components/Profile/ProfileInfo.js
+++ src/components/Profile/ProfileInfo.js
@@ -2,6 +2,15 @@ import React from "react";
 import "./ProfileInfo.css";
 
 export default function ProfileInfo(props) {
+  function birthdate(date) {
+    const dateObj = new Date(date); //returned from mysql timestamp/datetime field
+    const month = dateObj.getUTCMonth() + 1; //months from 1-12
+    const day = dateObj.getUTCDate();
+    const year = dateObj.getUTCFullYear();
+    const newdate = day + "/" + month + "/" + year;
+    return newdate;
+  }
+
   return (
     <div className="mainSection">
       <div className="infoBlock">
@@ -27,7 +36,7 @@ export default function ProfileInfo(props) {
               <tr>
                 <td>Date of Birth</td>
                 <td className="studentDob">
-                  {props.selectedStudent.date_of_birth}
+                  {birthdate(props.selectedStudent.date_of_birth)}
                 </td>
               </tr>
               <tr>
