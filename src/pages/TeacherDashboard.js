0516431af04676a6dcd34074b5298c6c560773c2
Caedan <caedanlavender@gmail.com>
2021-07-20 07:52:30 +1200

    Removed testing 'extras' from Teacher Dashboard
    Had props.xxx in random places to test the props were coming through properly

--- src/pages/TeacherDashboard.js
+++ src/pages/TeacherDashboard.js
@@ -216,7 +216,7 @@ export const TeacherDashboard = (props) => {
 						<div className="panel--right__buttonContainer">
 							<Link>
 								<Button className={global.orangeButton} variant="contained">
-									Take Screenshot{props.user.name}
+									Take Screenshot
 								</Button>
 							</Link>
 							<Link>
