acebbc611fd2f8300f7a3880e4dd16b3e74a471a
Caedan <caedanlavender@gmail.com>
2021-07-21 16:51:41 +1200

    Reintroduced testing automation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ProjectView from './pages/ProjectView';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText("Home");
+	expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Project View', () => {
+	render(<ProjectView />);
+	const linkElement = screen.getByText("Beginner");
+	expect(linkElement).toBeInTheDocument();
 });
