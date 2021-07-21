222b627a03da5c9ca6fd0f794a872f26cd13b22c
Caedan <caedanlavender@gmail.com>
2021-07-22 09:59:27 +1200

    Added conditions for Make and Preview
    Renders image from /public after pulling path from database

--- src/components/projectView/ProjectItem/ProjectDashboardContent.js
+++ src/components/projectView/ProjectItem/ProjectDashboardContent.js
@@ -17,15 +17,20 @@ const ProjectDashboardContent = ({ tab, project }) => {
 		case 'Instructions':
 			return (
 				<div className="project__instructions">
-					<div dangerouslySetInnerHTML={{__html: project.instructions}}/>
-					<h1 className="heading-centered">Congratulations!<br/>You Completed {project.project_number}!</h1>
+					<div dangerouslySetInnerHTML={{ __html: project.instructions }} />
+					<h1 className="heading-centered">Congratulations!<br />You Completed {project.project_number}!</h1>
 				</div>
 				// <iframe title="vimeo-player" src="https://player.vimeo.com/video/65583694" width="640" height="400" frameborder="0" allowfullscreen></iframe>
 			)
-			case 'Video Tutorial':
-				return (
-					<iframe title="vimeo-player" src={project.video} width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
-				)
+		case 'Video Tutorial':
+			return (
+				<iframe title="vimeo-player" src={project.video} width="100%" height="100%" frameborder="0" allowfullscreen></iframe>
+			)
+		case 'Make Project':
+		case 'Preview Project':
+			return(
+				<img src={project.project_preview} width="80%" alt={tab}/>
+			)
 		default:
 			return (
 				<p>{tab}</p>
