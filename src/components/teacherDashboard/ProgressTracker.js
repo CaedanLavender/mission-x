da6cecec524a42a98adce4bda73a232841602b61
Caedan <caedanlavender@gmail.com>
2021-07-27 07:32:52 +1200

    Set up axios to endpoint for progress data

--- src/components/teacherDashboard/ProgressTracker.js
+++ src/components/teacherDashboard/ProgressTracker.js
@@ -1,12 +1,52 @@
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './ProgressTracker.css';
 
 const ProgressTracker = () => {
+	const [projectCount, setProjectCount] = useState();
+	const [progressData, setProgressData] = useState([]);
 
-	axios.get()
+
+	const fetchProjectCount = () => {
+		axios.get('http://localhost:4000/count?table=project')
+			.then((res) => {
+				console.log(res.data.count)
+				setProjectCount(res.data.count)
+			})
+			.catch(() => console.log("There was a catch error retrieving the project count"))
+	}
+
+	const fetchProjectProgress = () => {
+		axios.get('http://localhost:4000/progress')
+			.then(res => {
+				console.log(res.data)
+				setProgressData(res.data)
+			})
+			.catch(() => console.log("There was a catch error retrieving the progress data"))
+	}
+
+	useEffect(() => {
+		fetchProjectCount();
+		fetchProjectProgress();
+	}, []);
 
 	return (
-		<p>Test</p>
+		<div className="progress-tracker-super">
+			{
+
+			}
+			<div className="progress-tracker-container" >
+				<div className="progress-tracker__text-container">
+					<div className="progress-tracker__title">Aiden Andrews</div>
+					<div className="progress-tracker__subtitle">4/15 Projects Completed</div>
+				</div>
+				<div className="progress-tracker__dot-container">
+					<div className="progress-tracker__dot">
+						1
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 }
 
